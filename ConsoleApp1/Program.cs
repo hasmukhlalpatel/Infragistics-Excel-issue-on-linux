03e170713aa30d2d80f35c11c6596fb3afe13dfe
Hasmukh Patel <hasmukh77.patel@gmail.com>
2024-08-09 18:56:58 +0200

    issue file sample

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Infragistics.Documents.Excel;
+
+Console.WriteLine("Hello, World!");
+Workbook workbook1 = new Workbook();
+Worksheet worksheet1 = workbook1.Worksheets.Add("Sheet 1");
+worksheet1.Rows[0].Cells[0].Value = 42;
+worksheet1.Columns[0].AutoFitWidth();
+workbook1.Save("Workbook1.xls");
---
+++ readme.md
@@ -0,0 +1,2 @@
+
+$ docker buildx  build -t my-img:1 -f ./ConsoleApp1/Dockerfile  .
